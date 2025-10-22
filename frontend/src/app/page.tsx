f56bc689169694ca18087f1fd8a280657f7b314b
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-10-22 00:55:50 -0400

    image centering

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
+import "../styles/landing.css";
 
 export default function Home() {
   return (
     <>
-      <header></header>
+      <header>
+        <h2>Welcome to the</h2>
+        <h1>Gal Senior Care Scam Simulator</h1>
+      </header>
       <main>
+        <Image 
+          className="landing-img"
+          src="/images/landing_page_img.png"
+          alt="Landing Page Image"
+          width={800}
+          height={600}
+        />
+        <button>Go to Modules</button>
       </main>
-      <footer></footer>
+      <footer>Created by</footer>
     </>
   );
 }
