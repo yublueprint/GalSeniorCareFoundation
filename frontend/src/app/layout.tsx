0d21f21aba87f8b286f2f663ef52f1ea38034258
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-10-30 20:25:41 -0400

    get inter font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700', '800'],
+  variable: "--font-inter",
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +18,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
