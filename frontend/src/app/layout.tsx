b4c0e17e6ffce5dd825fa6547cecefd124431173
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-10-30 20:28:27 -0400

    fix font setup

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
 
@@ -8,17 +8,6 @@ const inter = Inter({
   weight: ['400', '500', '600', '700', '800'],
   variable: "--font-inter",
 })
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <AuthProvider>{children}</AuthProvider>
       </body>
