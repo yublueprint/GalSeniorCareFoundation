f462c73b1d4e5c43472e939999dc48f0519a49a6
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-11-22 23:04:34 -0500

    insert elements to page

---
+++ frontend/src/app/module-landing/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+import * as Form from "@radix-ui/react-form";
+import { FormEvent, use, useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/dist/client/link";
+
+export default function moduleLandingPage() {
+    const router = useRouter();
+
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+              <div className="absolute left-6 top-6">
+                <Image src="/galLogo.svg" alt="galLogo" width={100} height={100} />
+              </div>
+        
+              <main className="w-full px-4">
+                <h2>Welcome to the </h2> 
+                <h1>Gal Senior Care Scam Simulator</h1>
+
+                <div className="video-container"/>
+
+                <Form.Submit asChild>
+                    <button
+                    type="submit"
+                    className="cursor-pointer mt-6 w-full py-2 rounded-md bg-[#f1bb79] shadow-[3px_3px_0_#d09a58] font-bold text-xl hover:bg-[#f1bb79]/85 disabled:opacity-60"
+                    >   
+                        Go to Modules
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="15" 
+                        height="15" 
+                        viewBox="0 0 15 15" 
+                        fill="none"
+                        className="arrow-icon"
+                        >
+                            <path d="M1 6.36395C0.447715 6.36395 0 6.81167 0 7.36395C0 7.91624 0.447715 8.36395 1 8.36395V7.36395V6.36395ZM14.7071 8.07106C15.0976 7.68054 15.0976 7.04737 14.7071 6.65685L8.34315 0.292885C7.95262 -0.0976395 7.31946 -0.0976395 6.92893 0.292885C6.53841 0.683409 6.53841 1.31657 6.92893 1.7071L12.5858 7.36395L6.92893 13.0208C6.53841 13.4113 6.53841 14.0445 6.92893 14.435C7.31946 14.8255 7.95262 14.8255 8.34315 14.435L14.7071 8.07106ZM1 7.36395V8.36395H14V7.36395V6.36395H1V7.36395Z" 
+                            fill="currentColor"/>
+                        </svg>
+                    </button>
+                </Form.Submit>
+              </main>
+
+            <div className="absolute right-4 bottom-4 text-xl flex items-center gap-2 font-medium">
+                Created by{" "}
+                <Link href="https://yublueprint.org/">
+                    <Image
+                        src="/myblueprint.svg"
+                        alt="myblueprint"
+                        width={40}
+                        height={20}
+                    />
+                </Link>
+            </div>
+        </div>
+    );
+}
